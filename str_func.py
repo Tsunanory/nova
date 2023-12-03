f5a0cbb80318221afcc45ed52c942b270bced6f7
Tsunanory <144465058+Tsunanory@users.noreply.github.com>
2023-12-03 10:19:02 +0100

    Update str_func.py
    capitalization added

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
-def str_func(str):
+def str_upper(str):
+    '''Возвращаем слово капсом'''
     return str.upper()
+
+def str_capital(str):
+    '''Слово с большой буквы'''
+    return str.capitalize()
